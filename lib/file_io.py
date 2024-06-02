a0a598ad4dcde1e29e2197a3754d3ac335935b40
Samson Mubeya <rasspartah@gmail.com>
2024-06-02 23:27:41 +0300

    learnt io with encoding utf8

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,18 @@
 def write_file(file_name, file_content):
+    
+    with open(f"{file_name}.txt",mode='w', encoding="utf-8") as logfile:
+        logfile.write(file_content)
+       
     pass
 
 def append_file(file_name, append_content):
+    with open(f"{file_name}.txt",mode='a', encoding="utf-8") as logfile:
+        logfile.write(append_content)
     pass
 
 def read_file(file_name):
+    with open(f"{file_name}.txt") as logfile:
+        return logfile.read()
+        
     pass
+
